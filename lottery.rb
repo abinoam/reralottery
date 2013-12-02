423545856b70a44d28fac7e5a330c4d0677278df
Abinoam Jr <abinoam@gmail.com>
2013-12-02 16:03:49 -0300

    Remove all now unecessary to_i conversion

--- lottery.rb
+++ lottery.rb
@@ -8,18 +8,18 @@ list = []
 (list << (rand(99999)+1)).uniq! while list.length < 5
 number1, number2, number3, number4, number5 = list
 serie1, serie2, serie3, serie4, serie5 = Array.new(5) { rand(180)+1 }
-number6 = number1.to_i - 1
-number7 = number1.to_i + 1
-number8 = number2.to_i - 1
-number9 = number2.to_i + 1
-number10 = number3.to_i - 1
-number11 = number3.to_i + 1
-number12 = number1.to_i / 100
-number13 = number2.to_i / 100
-number14 = number3.to_i / 100
-number15 = number4.to_i / 100
-number16 = number5.to_i / 100
-number17 = number1.to_i % 10
+number6 = number1 - 1
+number7 = number1 + 1
+number8 = number2 - 1
+number9 = number2 + 1
+number10 = number3 - 1
+number11 = number3 + 1
+number12 = number1 / 100
+number13 = number2 / 100
+number14 = number3 / 100
+number15 = number4 / 100
+number16 = number5 / 100
+number17 = number1 % 10
 puts list
 
 puts "\n" #break
@@ -48,47 +48,47 @@ tkt_amount.times do
   prize8 = 100 * quantity1 #Cantena
   prize9 = 20 * quantity1 #Reintegro
 
-  if ticket1 == number1.to_i
+  if ticket1 == number1
     show_results(ticket1, prize1)
   end
 
-  if ticket1 == number2.to_i
+  if ticket1 == number2
     show_results(ticket1, prize2)
   end
 
-  if ticket1 == number3.to_i
+  if ticket1 == number3
     show_results(ticket1, prize3)
   end
 
-  if ticket1 == number4.to_i or
-    ticket1 == number5.to_i
+  if ticket1 == number4 or
+    ticket1 == number5
     show_results(ticket1, prize4)
   end
 
-  if ticket1 == number6.to_i or
-    ticket1 == number7.to_i
+  if ticket1 == number6 or
+    ticket1 == number7
     show_results(ticket1, prize5)
   end
 
-  if ticket1 == number8.to_i or
-    ticket1 == number9.to_i
+  if ticket1 == number8 or
+    ticket1 == number9
     show_results(ticket1, prize6)
   end
 
-  if ticket1 == number10.to_i or
-    ticket1 == number11.to_i
+  if ticket1 == number10 or
+    ticket1 == number11
     show_results(ticket1, prize7)
   end
 
-  if ticket1.to_i / 100 == number12.to_i or
-    ticket1.to_i / 100 == number13.to_i or
-    ticket1.to_i / 100 == number14.to_i or
-    ticket1.to_i / 100 == number15.to_i or
-    ticket1.to_i / 100 == number16.to_i
+  if ticket1 / 100 == number12 or
+    ticket1 / 100 == number13 or
+    ticket1 / 100 == number14 or
+    ticket1 / 100 == number15 or
+    ticket1 / 100 == number16
     show_results(ticket1, prize8)
   end
 
-  if ticket1.to_i % 10 == number17.to_i
+  if ticket1 % 10 == number17
     show_results(ticket1, prize9)
   end
 end
