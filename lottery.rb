be634a63851acd25631dcffb9c10f427f590dc54
Abinoam Jr <abinoam@gmail.com>
2013-12-02 15:08:17 -0300

    Patch with second version in thread by Re Ra

--- lottery.rb
+++ lottery.rb
@@ -2,7 +2,7 @@
 #coding: utf-8
 
 #ticket database
-ticket1 = 88795 #ticket number
+ticket1 = 23453 #ticket number
 quantity1 = 3  #quantity bought
 
 puts "\n" #break
@@ -28,51 +28,62 @@ puts list
 
 puts "\n" #break
 
+#prizes
+prize1 = 400000 * quantity1 #1st
+prize2 = 125000 * quantity1 #2n
+prize3 = 50000 * quantity1 #3rd
+prize4 = 20000 * quantity1 #4th
+prize5 = 2000 * quantity1 #Consolation1
+prize6 = 1250 * quantity1 #Consolation2
+prize7 = 960 * quantity1 #Consolation3
+prize8 = 100 * quantity1 #Cantena
+prize9 = 20 * quantity1 #Reintegro
+
 #winnings
+def show_results(a, b)
+  puts "Ticket ##{a.to_s.rjust(5, '0')} won €#{b}."
+end
+
 if ticket1 == number1.to_i
-  prize1 = 400000 * quantity1
-  puts "Ticket ##{ticket1.to_s.rjust(5, '0')} won €#{prize1}."
+  show_results(ticket1, prize1)
 end
+
 if ticket1 == number2.to_i
-  prize2 = 125000 * quantity1
-  puts "Ticket ##{ticket1.to_s.rjust(5, '0')} won €#{prize2}."
+  show_results(ticket1, prize2)
 end
+
 if ticket1 == number3.to_i
-  prize3 = 50000 * quantity1
-  puts "Ticket ##{ticket1.to_s.rjust(5, '0')} won €#{prize3}."
+  show_results(ticket1, prize3)
 end
-if ticket1 == number4.to_i
-  prize4 = 20000 * quantity1
-  puts "Ticket ##{ticket1.to_s.rjust(5, '0')} won €#{prize4}."
+
+if ticket1 == number4.to_i or
+  ticket1 == number5.to_i
+  show_results(ticket1, prize4)
 end
-if ticket1 == number5.to_i
-  prize5 = 20000 * quantity1
-  puts "Ticket ##{ticket1.to_s.rjust(5, '0')} won €#{prize5}."
-end
+
 if ticket1 == number6.to_i or
   ticket1 == number7.to_i
-  prize6 = 2000 * quantity1
-  puts "Ticket ##{ticket1.to_s.rjust(5, '0')} won €#{prize6}."
+  show_results(ticket1, prize5)
 end
+
 if ticket1 == number8.to_i or
   ticket1 == number9.to_i
-  prize7 = 1250 * quantity1
-  puts "Ticket ##{ticket1.to_s.rjust(5, '0')} won €#{prize7}."
+  show_results(ticket1, prize6)
 end
+
 if ticket1 == number10.to_i or
   ticket1 == number11.to_i
-  prize8 = 960 * quantity1
-  puts "Ticket ##{ticket1.to_s.rjust(5, '0')} won €#{prize8}."
+  show_results(ticket1, prize7)
 end
+
 if ticket1.to_i / 100 == number12.to_i or
   ticket1.to_i / 100 == number13.to_i or
   ticket1.to_i / 100 == number14.to_i or
   ticket1.to_i / 100 == number15.to_i or
   ticket1.to_i / 100 == number16.to_i
-  prize9 = 100 * quantity1
-  puts "Ticket ##{ticket1.to_s.rjust(5, '0')} won €#{prize9}."
+  show_results(ticket1, prize8)
 end
+
 if ticket1.to_i % 10 == number17.to_i
-  prize10 = 20 * quantity1
-  puts "Ticket ##{ticket1.to_s.rjust(5, '0')} won €#{prize10}."
+  show_results(ticket1, prize9)
 end
